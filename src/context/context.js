8c00b64b440847ed0408a8914f553de078801b46
ruslanepi <ruslanep2@gmail.com>
2021-09-04 23:50:20 +0300

    added search functionality and getting user functions

--- src/context/context.js
+++ src/context/context.js
@@ -15,8 +15,62 @@ const GithubProvider = ({ children }) => {
   const [repos, setRepos] = useState(mockRepos)
   const [followers, setFollowers] = useState(mockFollowers)
 
+  //requests loading
+  const [requests, setRequests] = useState(0)
+  const [loading, setIsLoading] = useState(false)
+
+  const [errors, setErrors] = useState({ show: false, msg: '' })
+
+  const searchGithubUser = async (user) => {
+    toggleError()
+    //setLoading(true)
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    )
+
+    if (response) {
+      setGithubUser(response.data)
+      //more logic here
+    } else {
+      toggleError(true, 'there is no user with that username')
+    }
+  }
+
+  //check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data
+        setRequests(remaining)
+        if (remaining === 0) {
+          toggleError(
+            true,
+            'sorry, you have exceeded your houtly rate limits! '
+          )
+        }
+      })
+      .catch((err) => console.log(err))
+  }
+
+  function toggleError(show = false, msg = '') {
+    setErrors({ show, msg })
+  }
+  //error
+  useEffect(checkRequests, [])
+
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        errors,
+        searchGithubUser,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   )
